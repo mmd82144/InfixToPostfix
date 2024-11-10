22a70c276f30b5ba7ca4b9039af240846bed7579
Mohammad Rezazadeh <mohammad1382144@gmail.com>
2024-11-10 20:00:58 +0330

    InfixToPostfix
    home work

---
+++ InfixToPostfix.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static InfixToPostfixConvertersh.Form1;
+namespace InfixToPostfixConvertersh
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+public class InfixToPostfixConverter
+        {
+
+            private int GetPrecedence(char op)
+            {
+                switch (op)
+                {
+                    case '+':
+                    case '-':
+                        return 1;
+                    case '*':
+                    case '/':
+                        return 2;
+                    case '^':
+                        return 3;
+                    default:
+                        return 0;
+                }
+            }
+
+            private bool IsOperator(char c)
+            {
+                return c == '+' || c == '-' || c == '*' || c == '/' || c == '^';
+            }
+
+            public string Convert(string infix)
+            {
+                StringBuilder postfix = new StringBuilder();
+                Stack<char> stack = new Stack<char>();
+
+                foreach (char token in infix)
+                {
+                    if (char.IsWhiteSpace(token))
+                        continue;
+
+                    if (char.IsLetterOrDigit(token))
+                    {
+                        postfix.Append(token);
+                    }
+                    else if (IsOperator(token))
+                    {
+                        while (stack.Count > 0 && GetPrecedence(stack.Peek()) >= GetPrecedence(token))
+                        {
+                            postfix.Append(stack.Pop());
+                        }
+                        stack.Push(token);
+                    }
+                    else if (token == '(')
+                    {
+                        stack.Push(token);
+                    }
+                    else if (token == ')')
+                    {
+                        while (stack.Count > 0 && stack.Peek() != '(')
+                        {
+                            postfix.Append(stack.Pop());
+                        }
+                        stack.Pop(); // Remove '(' from stack
+                    }
+                }
+
+                while (stack.Count > 0)
+                {
+                    postfix.Append(stack.Pop());
+                }
+
+                return postfix.ToString();
+            }
+        }
+
+      
+
+        private void btnConvert_Click_1(object sender, EventArgs e)
+        {
+                string infixExpression = textBox1.Text;
+                InfixToPostfixConverter converter = new InfixToPostfixConverter();
+                string postfixExpression = converter.Convert(infixExpression);
+                textBox2.Text = postfixExpression;
+
+                PostfixEvaluator evaluator = new PostfixEvaluator();
+                int result = evaluator.Evaluate(postfixExpression); // در صورتی که بخواهید محاسبه کنید
+                label1.Text= ("نتیجه: " + result);
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox2.ReadOnly = true;
+        }
+    }
+}
+public class PostfixEvaluator
+{
+    public int Evaluate(string postfix)
+    {
+        Stack<int> stack = new Stack<int>();
+
+        foreach (char token in postfix)
+        {
+            if (char.IsDigit(token))
+            {
+                stack.Push(token - '0'); // تبدیل به عدد
+            }
+            else
+            {
+                int operand2 = stack.Pop();
+                int operand1 = stack.Pop();
+                switch (token)
+                {
+                    case '+':
+                        stack.Push(operand1 + operand2);
+                        break;
+                    case '-':
+                        stack.Push(operand1 - operand2);
+                        break;
+                    case '*':
+                        stack.Push(operand1 * operand2);
+                        break;
+                    case '/':
+                        stack.Push(operand1 / operand2);
+                        break;
+                }
+            }
+        }
+
+        return stack.Pop();
+    }
+}
---
+++ WindowsFormsApplication10/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace InfixToPostfixConvertersh
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(12, 98);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(116, 53);
+            this.btnConvert.TabIndex = 0;
+            this.btnConvert.Text = "محاسبه";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(228, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "جواب";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 70);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(141, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(159, 70);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(141, 22);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(202, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "لطفا عدد اول را به صورت زیر قرار دهید";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(32, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 32);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "x+y";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(312, 231);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnConvert);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
